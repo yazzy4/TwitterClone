57cbab8478cb1fd482e4472c646a2430c06a6e49
Yaz Burrell <yburrell@zillion.com>
2022-01-18 13:54:52 -0500

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Yaz Burrell on 1/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+    }
+
+
+}
+
