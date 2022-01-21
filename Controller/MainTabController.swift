f22f28f78cf09043eb9408c5da207a6b2f3cc5fa
Yaz Burrell <yburrell@zillion.com>
2022-01-21 15:39:22 -0500

    added sticky button to main tab bar and extension helper for views

--- Controller/MainTabController.swift
+++ Controller/MainTabController.swift
@@ -11,6 +11,14 @@ class MainTabController: UITabBarController {
     
     //MARK: - Properties
     
+    let actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.backgroundColor = .blue
+        button.setImage(UIImage(named: "new_tweet"), for: .normal)
+        return button
+    }()
+    
     //MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -18,12 +26,18 @@ class MainTabController: UITabBarController {
         
         navigationController?.setNavigationBarHidden(false, animated: true)
         configureViewControllers()
-        
-        
+        configureUI()
         
     }
     
     //MARK: - Helpers
+    
+    func configureUI() {
+        view.addSubview(actionButton)
+        actionButton.anchor( bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingBottom: 64, paddingRight: 16, width: 56, height: 56)
+        actionButton.layer.cornerRadius = 56 / 2
+        
+    }
     
     func configureViewControllers() {
         
