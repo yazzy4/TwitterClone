f589f3d33c8556e1c41ce2fb56f7adb6176957bf
Yaz Burrell <yburrell@zillion.com>
2022-01-24 16:22:57 -0500

    added background colors to view and image to login view

---
+++ Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,29 @@
+//
+//  RegistrationController.swift
+//  TwitterClone
+//
+//  Created by Yaz Burrell on 1/24/22.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+}
