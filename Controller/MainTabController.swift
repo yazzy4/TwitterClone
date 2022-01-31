66f289e0267e8a66ee737b0d0393ab9f1c451f28
Yaz Burrell <yburrell@zillion.com>
2022-01-31 16:12:58 -0500

    added method to handle user logout

--- Controller/MainTabController.swift
+++ Controller/MainTabController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class MainTabController: UITabBarController {
     
@@ -31,10 +32,34 @@ class MainTabController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationController?.setNavigationBarHidden(false, animated: true)
-        configureViewControllers()
-        configureUI()
-        
+        //logUserOut()
+        view.backgroundColor = .twitterBlue
+        authenticateUserAndConfigureUI()
+    }
+    
+    // MARK: - API
+    
+    func authenticateUserAndConfigureUI() {
+        if Auth.auth().currentUser ==  nil {
+            DispatchQueue.main.async {
+                let nav = UINavigationController(rootViewController: LoginController())
+                nav.modalPresentationStyle = .fullScreen
+                self.present(nav, animated: true, completion: nil)
+            }
+            print("DEBUG: User is NOT logged in")
+        } else {
+            configureViewControllers()
+            configureUI()
+            print("DEBUG: User is logged in")
+        }
+    }
+    
+    func logUserOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print("DEBUG: Failed to sign out with error: \(error.localizedDescription)")
+        }
     }
     
     //MARK: - Helpers
