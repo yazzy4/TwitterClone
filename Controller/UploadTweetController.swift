21dff0891f84eceaa46db9156f1ee03b4831d862
Yaz Burrell <yburrell@zillion.com>
2022-02-10 14:05:41 -0500

    added cached profile image to upload tweet controller

--- Controller/UploadTweetController.swift
+++ Controller/UploadTweetController.swift
@@ -11,6 +11,8 @@ class UploadTweetController: UIViewController {
     
     // MARK: - Properties
     
+    private let user: User
+    
     private lazy var actionButton: UIButton = {
         let button =  UIButton(type: .system)
         button.backgroundColor = .twitterBlue
@@ -27,12 +29,31 @@ class UploadTweetController: UIViewController {
         return button
     }()
     
+    private let profileImageView: UIImageView = {
+       let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
+    
     // MARK: - Lifecycle
     
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-     
         configureUI()
+
     }
     
     // MARK: - Selectors
@@ -51,14 +72,21 @@ class UploadTweetController: UIViewController {
     
     func configureUI() {
         view.backgroundColor = .white
-        //navigationController?.navigationBar.barTintColor = .black
-        //navigationController?.navigationBar.isTranslucent = true
+        configureNavBar()
         
+        view.addSubview(profileImageView)
+        profileImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                                left: view.leftAnchor,
+                                paddingTop: 16,
+                                paddingLeft:  16)
+        
+        profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
+    }
+    
+    func configureNavBar() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleCancel))
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: actionButton)
     }
-    
-    
     
     
 }
