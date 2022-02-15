fdc698778c070e04cb8a36c456aef91ce91cdad0
Yaz Burrell <yburrell@zillion.com>
2022-02-15 16:37:14 -0500

    handled placeholder text for caption view

--- View/CaptionTextView.swift
+++ View/CaptionTextView.swift
@@ -15,6 +15,7 @@ class CaptionTextView: UITextView {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 16)
         label.textColor = .darkGray
+        label.text = "testing testing"
         return label
     }()
     
@@ -22,7 +23,7 @@ class CaptionTextView: UITextView {
     override init(frame: CGRect, textContainer: NSTextContainer?) {
         super.init(frame: frame, textContainer: textContainer)
         
-        backgroundColor = .black
+        backgroundColor = .white
         font = UIFont.systemFont(ofSize: 16)
         heightAnchor.constraint(equalToConstant: 300).isActive = true
         
@@ -31,6 +32,8 @@ class CaptionTextView: UITextView {
                                 left: leftAnchor,
                                 paddingTop: 8,
                                 paddingLeft: 4)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleTextInputChange), name: UITextView.textDidChangeNotification, object: nil)
     }
     
     required init?(coder: NSCoder) {
@@ -38,6 +41,11 @@ class CaptionTextView: UITextView {
     }
     
     // MARK: - Selectors
+    
+    @objc func handleTextInputChange() {
+        print("DEBUG: Hide and show placeholder text")
+        placeholderLabel.isHidden = !text.isEmpty
+    }
     
     // MARK: - API
     
