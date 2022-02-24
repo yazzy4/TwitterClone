a916af44be87358321b6902d30f9840ec9db65bf
Yaz Burrell <yburrell@zillion.com>
2022-02-24 17:06:26 -0500

    set up UI for feed with profile image and placeholder tweet and handle

--- View/TweetCell.swift
+++ View/TweetCell.swift
@@ -9,13 +9,63 @@ import UIKit
 
 class TweetCell: UICollectionViewCell {
     
+    //MARK: - Properties
+    
+    private let profileImageView: UIImageView = {
+       let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.numberOfLines = 0
+        label.text = "Test Caption"
+        return label
+    }()
+    
+    private let infoLabel = UILabel()
+    
+    //MARK: - Lifecycle
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .red
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor,
+                                paddingTop: 12, paddingLeft: 8)
+        
+        let stack = UIStackView(arrangedSubviews: [infoLabel, captionLabel])
+        stack.axis = .vertical
+        stack.distribution = .fillProportionally
+        stack.spacing = 4
+        
+        addSubview(stack)
+        stack.anchor(top: profileImageView.topAnchor, left: profileImageView.rightAnchor,
+                     right: rightAnchor, paddingLeft: 12, paddingRight: 12)
+        
+        infoLabel.font = UIFont.systemFont(ofSize: 14)
+        infoLabel.text = "Tweeting at you @sp"
+        
+        let underlineView = UIView()
+        underlineView.backgroundColor = .systemGroupedBackground
+        addSubview(underlineView)
+        underlineView.anchor(top: topAnchor, left: leftAnchor,
+                             paddingTop: 12, paddingLeft: 8)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    //MARK: - Selectors
+    
+    //MARK: - Helpers
 }
