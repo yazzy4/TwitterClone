9daaecc90c9b04996ee8d90ff88ed96bbf153fb6
Yaz Burrell <yazburrell@pursuit.org>
2022-03-03 16:21:11 -0500

    set up initial collection view for profile view

--- Controller/ProfileController.swift
+++ Controller/ProfileController.swift
@@ -8,6 +8,8 @@
 import Foundation
 import UIKit
 
+private let reuseIdentifier = "TweetCell"
+
 class ProfileController: UICollectionViewController {
     
     // MARK: - Properties
@@ -16,11 +18,48 @@ class ProfileController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .twitterBlue
+        configureCollectionView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    // MARK: - Helpers
+    
+    func configureCollectionView() {
+        collectionView.register(TweetCell.self, forCellWithReuseIdentifier: reuseIdentifier)
     }
     
    
     
     
     
+}
+
+// MARK: - UICollectionViewDataSource
+ 
+extension ProfileController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TweetCell
+        return cell
+    }
+ 
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension ProfileController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 300)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 120)
+    }
 }
