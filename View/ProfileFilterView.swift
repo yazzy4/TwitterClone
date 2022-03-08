69269232b03442101b17711bcecb29520eb20d97
Yaz Burrell <yazburrell@pursuit.org>
2022-03-08 13:59:42 -0500

    creating profile filter view

---
+++ View/ProfileFilterView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileFilterView.swift
+//  TwitterClone
+//
+//  Created by Yaz Burrell on 3/8/22.
+//
+
+import Foundation
+import UIKit
+
+class ProfileFilterView: UIView {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+}
