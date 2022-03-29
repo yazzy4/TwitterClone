55f15b34219b2851929095373439ffcf98951632
Yaz Burrell <yazburrell@pursuit.org>
2022-03-29 16:06:28 -0400

    Follow updating in Firebase

--- API/UserService.swift
+++ API/UserService.swift
@@ -6,6 +6,7 @@
 //
 
 import Firebase
+import UIKit
 
 struct UserService {
     static let shared = UserService()
@@ -31,12 +32,14 @@ struct UserService {
         }
     }
     
-    func followUser(uid: String, completion: @escaping(Database, Error?) -> Void) {
+    func followUser(uid: String, completion: @escaping( Error?, DatabaseReference) -> Void) {
         guard let currentUid = Auth.auth().currentUser?.uid else { return }
         
         print("DEBUG: Current uid is \(currentUid) started following \(uid)")
         print("DEBUG: Uid \(uid) gained \(currentUid) as a follower")
         
-        //REF_USER_FOLLOWING.child(currentUid).updateChildValues(<#T##values: [AnyHashable : Any]##[AnyHashable : Any]#>)
+        REF_USER_FOLLOWING.child(currentUid).updateChildValues([uid : 1]) { err, ref in
+            REF_USER_FOLLOWERS.child(uid).updateChildValues([currentUid : 1], withCompletionBlock: completion)
+        }
     }
 }
