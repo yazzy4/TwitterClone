124a7130476182948fb06efd168a9197de3537ea
Yaz Burrell <yazburrell@pursuit.org>
2022-04-27 17:00:16 -0400

    setting up action sheet launcher in util

---
+++ Utils/ActionSheetLauncher.swift
@@ -0,0 +1,26 @@
+//
+//  ActionSheetLauncher.swift
+//  TwitterClone
+//
+//  Created by Yaz Burrell on 4/27/22.
+//
+
+import Foundation
+
+class ActionSheetLauncher: NSObject {
+    
+    // MARK: - Properties
+    
+    private let user: User
+    
+    init(user: User) {
+        self.user = user
+        super.init()
+    }
+    
+    // MARK: - Helpers
+    
+    func show() {
+        
+    }
+}
