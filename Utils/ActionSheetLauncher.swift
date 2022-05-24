e337f345e1f473ef83388c51460941b7a6725f2c
Yaz Burrell <yazburrell@pursuit.org>
2022-05-24 14:22:37 -0400

    Set up data model and UI for action sheet

--- Utils/ActionSheetLauncher.swift
+++ Utils/ActionSheetLauncher.swift
@@ -6,21 +6,120 @@
 //
 
 import Foundation
+import UIKit
+
+private let reuseIdentifier = "ActionSheetCell"
 
 class ActionSheetLauncher: NSObject {
     
     // MARK: - Properties
     
     private let user: User
+    private let tableView = UITableView()
+    private var window: UIWindow?
+    private lazy var viewModel = ActionSheetViewModel(user: user)
+    
+    private lazy var blackView: UIView = {
+        let view = UIView()
+        view.alpha = 0
+        view.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleDismissal))
+        view.addGestureRecognizer(tap)
+        return view
+    }()
+    
+    private lazy var cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Cancel", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.setTitleColor(.blue, for: .normal)
+        button.backgroundColor = .systemGroupedBackground
+        button.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var footerView: UIView = {
+        let view = UIView()
+        
+        view.addSubview(cancelButton)
+        cancelButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        cancelButton.anchor(left: view.leftAnchor, right: view.rightAnchor,
+                                paddingLeft: 12, paddingRight: 12)
+        cancelButton.centerY(inView: view)
+        cancelButton.layer.cornerRadius = 50 / 2
+        
+        return view
+    }()
     
     init(user: User) {
         self.user = user
         super.init()
+        
+        configureTableView()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleDismissal() {
+        UIView.animate(withDuration: 0.5) {
+            self.blackView.alpha = 0
+            self.tableView.frame.origin.y += 300
+        }
     }
     
     // MARK: - Helpers
+
     
     func show() {
+        print("DEBUG: Show action sheet for user \(user.username)")
         
+        guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
+        self.window = window
+        
+        window.addSubview(blackView)
+        blackView.frame = window.frame
+        
+        window.addSubview(tableView)
+        let height = CGFloat(viewModel.option.count * 60) + 100
+        tableView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
+        
+        UIView.animate(withDuration: 0.5) {
+            self.blackView.alpha = 1
+            self.tableView.frame.origin.y -= height
+        }
+    }
+    
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
+        tableView.layer.cornerRadius = 5
+        tableView.isScrollEnabled = false
+        
+        tableView.register(ActionSheetCell.self, forCellReuseIdentifier: reuseIdentifier)
+    }
+}
+
+
+extension ActionSheetLauncher: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.option.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! ActionSheetCell
+        cell.option = viewModel.option[indexPath.row]
+        return cell
+    }
+}
+
+extension ActionSheetLauncher: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return footerView
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 60
     }
 }
